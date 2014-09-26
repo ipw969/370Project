5ca2eac15edd02f063b41bd67fe02cdd5b5b5e04
Iain Workman <iain.workman@googlemail.com>
2014-09-26 11:13:54 -0600

    Added: - database.java ~ Will form basis of Database API
    Altered:
    - MovieScheduler.java ~ Added initialization logic for main system. Tried to register JDBC postgres diver. If any init stages fail structure is setup to show a dialog then gracefully exit

--- MovieScheduler/src/moviescheduler/MovieScheduler.java
+++ MovieScheduler/src/moviescheduler/MovieScheduler.java
@@ -5,6 +5,8 @@
  */
 package moviescheduler;
 
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
 /**
  *
  *
@@ -12,11 +14,33 @@ package moviescheduler;
 public class MovieScheduler {
 
     /**
-     * @param args the command line arguments
+     * Main entry point for the GUI of the movie scheduler system.
+     * @param args the command line arguments (unused)
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        // Meow meow meow
-    }
-    
+        // initialze sub systems. If fail any of the required inits, then
+        // initializedProperly is set to false
+        boolean initializedProperly = true;
+        // Each time an init action fails, add an error message to this list
+        ArrayList<String> errorsEncountered = new ArrayList();
+        
+        // Attempt initialization of database driver
+        try {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (ClassNotFoundException ex)
+        {
+                initializedProperly = false;
+                errorsEncountered.add("Could not load database driver with "
+                        + "message: " + ex.toString());
+        }
+        
+        // One of this inits failed. Display an error message and exit
+        if(!initializedProperly)
+            JOptionPane.showMessageDialog(null, "Could not load the system," + 
+                    " with errors:\n" + errorsEncountered.toString(),
+                    "Error Loading System!", 0);
+        
+        //ToDo: Load main window here.
+    }    
 }
