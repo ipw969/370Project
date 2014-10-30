f89800d8b598cf14d217e7fb7b3b593f6f8d8c95
Iain Workman <iain.workman@googlemail.com>
2014-10-29 21:10:26 -0600

    Added: - Schedule ~ Class representing the whole schedule for the filming of the script.

--- MovieScheduler/src/businessobjects/SceneFilmingDate.java
+++ MovieScheduler/src/businessobjects/SceneFilmingDate.java
@@ -1,6 +1,9 @@
 package businessobjects;
 
 import java.util.GregorianCalendar;
+import java.util.Iterator;
+import datatypes.Volunteer;
+import datatypes.Equipment;
 /**
  * Class which represents the time interval that a scene is scheduled to
  * filmed in 
@@ -36,6 +39,57 @@ public class SceneFilmingDate extends BaseBusinessObject {
     public TimeInterval sceneShootingInterval()
     {
         return sceneShootingInterval;
+    }
+    
+    /**
+     * Whether the time of the scene filming date has a conflict with any of
+     * the availabilites of its volunteers or equipment, with the current time
+     * interval that the filming date is set for.
+     * @return True if any of the volunteers or equipment are not available on
+     * the interval that this SceneFilmingDate is scheduled for, False 
+     * otherwise.
+     */
+    public boolean hasConflict()
+    {
+        if(scene == null)
+            return false;
+        
+        if(sceneShootingInterval == null)
+            return false;
+        
+        if(!scene.hasVolunteers() && !scene.hasEquipment())
+            return true;
+        
+        boolean filmingDateHasConflict = false;
+        
+        Iterator<Volunteer> volunteerIterator = scene.volunteerIterator();
+        while(volunteerIterator.hasNext())
+        {
+            Volunteer currentVolunteer = volunteerIterator.next();
+            // Check volunteer availabilities against our time interval
+            /*
+            for (TimeInterval currentInterval : currentVolunteer.availabilities())
+            {
+                if(currentInterval.compareTo(currentInterval) == 0)
+                    filmingDateHasConflict = true;
+            }
+            */
+        }
+        
+        Iterator<Equipment> equipmentIterator = scene.equipmentIterator();
+        while(equipmentIterator.hasNext())
+        {
+            Equipment currentEquipment = equipmentIterator.next();
+            // Check equipment availablities against our timer interval
+            /*
+            for (TimeInterval currentInterval : currentEquipment.availabilities())
+            {
+                if(currentInterval.compareTo(currentInterval) == 0)
+                    filmingDateHasConflict = true;
+            }
+            */
+        }
+        return filmingDateHasConflict;
     }
     
     /**
