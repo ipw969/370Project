97580c4e37de1f9781f4519012274a6f4eba86e8
jrm120 <edw4rd@live.ca>
2014-11-19 14:07:01 -0600

    more stuff
    -made the EquipmentForm ui
    -altered equipment business object to have availabilities
    -made EquipmentForm accessible from the main menu
    -edited volunteerForm a bit
    -added comments in various places
    -cheers :)

--- MovieScheduler/src/businessobjects/Equipment.java
+++ MovieScheduler/src/businessobjects/Equipment.java
@@ -5,20 +5,24 @@ package businessobjects;
  *
  * @author johnmason
  */
-public class Equipment extends BaseBusinessObject {
+public class Equipment extends BaseBusinessObject 
+{
 
     String equipmentName;
     String ownerFirstName;
     String ownerLastName;
     String ownerEmail;
+    BusinessObjectList<TimeInterval> availability = new BusinessObjectList<>();
 
     //empty constructor
-    public Equipment() {
+    public Equipment() 
+    {
         setIsNew(true);
     }
 
     //full constructor for when the equipment is a rental
-    public Equipment(String equipmentName, String ownerFirstName, String ownerLastName, String ownerEmail) {
+    public Equipment(String equipmentName, String ownerFirstName, String ownerLastName, String ownerEmail) 
+    {
         this.equipmentName = equipmentName;
         this.ownerFirstName = ownerFirstName;
         this.ownerLastName = ownerLastName;
@@ -26,49 +30,84 @@ public class Equipment extends BaseBusinessObject {
         setIsNew(true);
     }
 
-    public void setEquipmentName(String equipmentName) {
+    //full constructor for when the equipment is a rental
+
+    public Equipment(String equipmentName, String ownerFirstName, String ownerLastName,
+            String ownerEmail, BusinessObjectList<TimeInterval> avail) 
+    {
+        this.equipmentName = equipmentName;
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.ownerEmail = ownerEmail;
+        this.availability = avail;
+
+        setIsNew(true);
+    }
+
+    public void setEquipmentName(String equipmentName) 
+    {
         this.equipmentName = equipmentName;
         setHasChanged(true);
     }
 
-    public void setOwnerFirstName(String ownerFirstName) {
+    public void setOwnerFirstName(String ownerFirstName) 
+    {
         this.ownerFirstName = ownerFirstName;
         setHasChanged(true);
     }
 
-    public void setOwnerLastName(String ownerLastName) {
+    public void setOwnerLastName(String ownerLastName) 
+    {
         this.ownerLastName = ownerLastName;
         setHasChanged(true);
     }
 
-    public void setOwnerEmail(String ownerEmail) {
+    public void setOwnerEmail(String ownerEmail) 
+    {
         this.ownerEmail = ownerEmail;
         setHasChanged(true);
     }
+    
+    public void addAvailability(TimeInterval avail)
+    {
+        this.availability.add(avail);
+        setHasChanged(true);
+    }
 
-    public String getEquipmentName() {
+    public String getEquipmentName() 
+    {
         return this.equipmentName;
     }
 
-    public String getOwnerFirstName() {
+    public String getOwnerFirstName() 
+    {
         return this.ownerFirstName;
     }
 
-    public String getOwnerLastName() {
+    public String getOwnerLastName() 
+    {
         return this.ownerLastName;
     }
 
-    public String getOwnerEmail() {
+    public String getOwnerEmail() 
+    {
         return this.ownerEmail;
     }
-
+   
+    public BusinessObjectList <TimeInterval> getAvailability()
+   {
+       return availability;
+   }
+    
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return equipmentName + "," + ownerLastName + "," + ownerFirstName;
     }
 
     @Override
-    public String toDescriptiveString() {
+    public String toDescriptiveString() 
+    {
         StringBuilder newString = new StringBuilder();
         newString.append("Equipment name: " + this.equipmentName + "\n");
         newString.append("Owners last name: " + this.ownerLastName + "\n");
@@ -79,7 +118,8 @@ public class Equipment extends BaseBusinessObject {
     }
 
     @Override
-    public BaseBusinessObject clone() throws CloneNotSupportedException {
+    public BaseBusinessObject clone() throws CloneNotSupportedException 
+    {
         Equipment cloneEquipment = (Equipment) super.clone();
         cloneEquipment.setEquipmentName(this.getEquipmentName());
         cloneEquipment.setOwnerLastName(this.getOwnerLastName());
@@ -90,7 +130,8 @@ public class Equipment extends BaseBusinessObject {
     }
 
     @Override
-    public void merge(BaseBusinessObject mergeObject) {
+    public void merge(BaseBusinessObject mergeObject) 
+    {
         if (mergeObject == null) {
             throw new RuntimeException("The given mergeObject was null for the volunteer merge.");
         } else if (!(mergeObject instanceof Scene)) {
--- MovieScheduler/src/ui/VolunteerForm.java
+++ MovieScheduler/src/ui/VolunteerForm.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author johnmason
  */
 package ui;
 import actions.SaveVolunteerAction;
@@ -121,7 +120,7 @@ public class VolunteerForm extends javax.swing.JFrame {
 
         jLabel4.setText("End");
 
-        add.setText("Add");
+        add.setText("Add Availability");
         add.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 addActionPerformed(evt);
