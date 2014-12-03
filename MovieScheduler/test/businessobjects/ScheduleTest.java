a4a201e8702ddf122b8589351a1c6d553a0ed34b
unknown <rajlaforge@hotmail.ca>
2014-12-03 15:24:44 -0600

    Getting rid of the auto generated code that appeared in it.

--- MovieScheduler/test/businessobjects/ScheduleTest.java
+++ MovieScheduler/test/businessobjects/ScheduleTest.java
@@ -21,36 +21,12 @@ public class ScheduleTest {
     
     public ScheduleTest() 
     {
+        testgetScheduleConflicts();
+        testgetScheduleFor();
+        testGetScenesFilmingDate();
         
     }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-    
-    
-    @Before
-    public void setUp() 
-    {
-        
-    }
-    
-    @After
-    public void tearDown() 
-    {
-        
-    }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
-    
     /**Test getScheduledConflicts statement level.
      *
      */
